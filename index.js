457611765ce32675fbaf25f0c0e1158b3933e2d0
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2023-08-08 21:57:59 -0300

    initial development

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const menuMobile = document.getElementById("menu-mobile")
+    
+    menuMobile.className === "menu-mobile-active" 
+    ? menuMobile.className = "menu-mobile" 
+    : menuMobile.className = "menu-mobile-active"  
+
+}
